9e0840b44d3087575febef66f3ae696eeffd0da6
MenoniJesus <menonifpr@gmail.com>
2025-04-06 11:30:59 -0300

    alteracoes da atv-17-03, referente aula

--- ATV-17-03/js/index.js
+++ ATV-17-03/js/index.js
@@ -3,24 +3,58 @@
 
 document.addEventListener("DOMContentLoaded", (event)=>{
         buscarInscritos();
+        construirModal();
+        fazoX();
+
+        const temaLocal = localStorage.getItem("tema") || "light";
+        document.body.setAttribute("data-theme", temaLocal);
+
+        const btAlterarTema = document.getElementById("btAlterarTema");
+        btAlterarTema.textContent = temaLocal == "dark" ? "Light" : "Dark";
 });
 
+function construirModal() {
+        const botaoSaibaMais = document.getElementById("saiba-mais");
+        const modal = document.getElementById("modal");
+        botaoSaibaMais.addEventListener("click", ()=>{
+                modal.classList.remove("hidden");
+        });
+
+        window.addEventListener("click", (e)=>{
+                if(e.target == modal) {
+                        modal.classList.add("hidden");
+                }
+        });
+}
+
+function fazoX(){
+        const botaoX = document.getElementById("fechar-modal");
+        const modal = document.getElementById("modal");
+        botaoX.addEventListener("click", ()=>{
+                modal.classList.add("hidden");
+        })
+}
+
 function alterarTema(){
-        //Dom -> document object model
         const tema = document.body.getAttribute("data-theme");
         const novoTema = tema == 'dark' ? 'light' : 'dark';
         document.body.setAttribute("data-theme", novoTema);
 
         const btAlterarTema = document.getElementById("btAlterarTema");
-        btAlterarTema.textContent = btAlterarTema.textContent == "Light" ? "Dark" : "Light";
+        btAlterarTema.textContent = novoTema == "dark" ? "Light" : "Dark";
+
+        localStorage.setItem("tema", novoTema);
 }
 
 function buscarInscritos(){
-        fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(res => {
+        //fetch("https://jsonplaceholder.typicode.com/users")
+        fetch("json/inscritos.json")
+        .then(res => res.json())
+        .then(res => {
                 const divInscritos = document.getElementById('inscritos');
                 res.forEach(user => {
                         const novoParagrafo = document.createElement("p");
-                        novoParagrafo.textContent = `Nome: ${user.name}`;
+                        novoParagrafo.textContent = `Nome: ${user.nome}`;
                         divInscritos.appendChild(novoParagrafo);
                 })
         });
