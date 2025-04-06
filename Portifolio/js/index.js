ae7f8f85b26ea27605e88de6109b76c281097e80
MenoniJesus <menonifpr@gmail.com>
2025-04-06 12:32:29 -0300

    refactor(portifolio): adicionado dark mode e arquivo para variables

--- Portifolio/js/index.js
+++ Portifolio/js/index.js
@@ -4,6 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("contact-form");
     const status = document.getElementById("status");
 
+    const temaSalvo = localStorage.getItem("tema");
+    if (temaSalvo) {
+        document.body.setAttribute("data-theme", temaSalvo);
+        atualizarIconeTema(temaSalvo);
+    }
+
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
@@ -17,6 +23,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function alterarTema() {
+    const tema = document.body.getAttribute("data-theme");
+    const novoTema = tema === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute("data-theme", novoTema);
+
+    localStorage.setItem("tema", novoTema);
+
+    atualizarIconeTema(novoTema);
+}
+
+function atualizarIconeTema(tema) {
+    const btAlterarTema = document.getElementById("btAlterarTema");
+    const img = btAlterarTema.querySelector("img");
+    img.src = tema === "dark" ? "img/sol.png" : "img/lua.png";
+    img.alt = tema === "dark" ? "Imagem do Light mode" : "Imagem do Dark mode";
+}
 
 let bntMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
